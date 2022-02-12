04999da65eb3097bda57598e1ae34d7e80c6aa91
Yusuke Kawamoto <novogrammer@gmail.com>
2022-02-12 16:37:28 +0900

    test-scenario-socketio.tsを追加

---
+++ tools/test-scenario-socketio.ts
@@ -0,0 +1,31 @@
+import { Server, Socket, Namespace } from "socket.io";
+import { createServer } from "http";
+import { io as ioc, Socket as ClientSocket } from "socket.io-client";
+
+const PORT = 3000;
+const NSP_CHAT = "/chat"
+
+
+async function mainAsync() {
+  console.log("initialize");
+  const httpServer = createServer();
+  const sio = new Server(httpServer);
+
+  await new Promise<void>((resolve,reject)=>{
+    httpServer.listen(PORT,resolve);
+  });
+  console.log("listening");
+  sio.of(NSP_CHAT);
+  const url = `ws://localhost:${PORT}${NSP_CHAT}`;
+  const clientSocket = ioc(url, { forceNew: true });
+  await new Promise<void>((resolve,reject)=>{
+    clientSocket.on("connect",()=>{
+      console.log("connect");
+      resolve();
+    })
+  });
+  clientSocket.close();
+  sio.close();
+
+}
+mainAsync();
